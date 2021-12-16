aca75773f050738fbd59511c007d9e712a9be24b
Petru Radu <radu.petru@live.com>
2021-12-16 12:44:28 +0200

    first commit

---
+++ Part1_Classification_VectorSpaces/w1_preprocessing.py
@@ -0,0 +1,43 @@
+import nltk                                # Python library for NLP
+from nltk.corpus import twitter_samples    # sample Twitter dataset from NLTK
+import matplotlib.pyplot as plt            # library for visualization
+import random                              # pseudo-random number generator
+
+# downloads sample twitter dataset.
+nltk.download('twitter_samples')
+
+# select the set of positive and negative tweets
+all_positive_tweets = twitter_samples.strings('positive_tweets.json')
+all_negative_tweets = twitter_samples.strings('negative_tweets.json')
+
+print('Number of positive tweets: ', len(all_positive_tweets))
+print('Number of negative tweets: ', len(all_negative_tweets))
+
+print('\nThe type of all_positive_tweets is: ', type(all_positive_tweets))
+print('The type of a tweet entry is: ', type(all_negative_tweets[0]))
+
+# Declare a figure with a custom size
+fig = plt.figure(figsize=(5, 5))
+
+# labels for the two classes
+labels = 'Positives', 'Negative'
+
+# Sizes for each slide
+sizes = [len(all_positive_tweets), len(all_negative_tweets)]
+
+# Declare pie chart, where the slices will be ordered and plotted counter-clockwise:
+plt.pie(sizes, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+
+# Equal aspect ratio ensures that pie is drawn as a circle.
+plt.axis('equal')
+
+# Display the chart
+plt.show()
+
+
+# print positive in greeen
+print('\033[92m' + all_positive_tweets[random.randint(0,5000)])
+
+# print negative in red
+print('\033[91m' + all_negative_tweets[random.randint(0,5000)])
