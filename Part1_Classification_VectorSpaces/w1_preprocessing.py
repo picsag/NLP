e3bb54808b17489366a6ba9e13e18d4194a202e0
Petru Radu <radu.petru@live.com>
2021-12-20 23:34:21 +0200

    started utility functions

--- Part1_Classification_VectorSpaces/w1_preprocessing.py
+++ Part1_Classification_VectorSpaces/w1_preprocessing.py
@@ -1,7 +1,13 @@
 import nltk                                # Python library for NLP
 from nltk.corpus import twitter_samples    # sample Twitter dataset from NLTK
-import matplotlib.pyplot as plt            # library for visualization
+from nltk.corpus import stopwords          # module for stop words that come with NLTK
+from nltk.stem import PorterStemmer        # module for stemming
+from nltk.tokenize import TweetTokenizer   # module for tokenizing strings
+
 import random                              # pseudo-random number generator
+import matplotlib.pyplot as plt            # library for visualization
+import re                                  # library for regular expression operations
+import string                              # for string operations
 
 # downloads sample twitter dataset.
 nltk.download('twitter_samples')
@@ -41,3 +47,78 @@ print('\033[92m' + all_positive_tweets[random.randint(0,5000)])
 
 # print negative in red
 print('\033[91m' + all_negative_tweets[random.randint(0,5000)])
+
+tweet = all_positive_tweets[2277]
+print(tweet)
+
+# download the stopwords from NLTK
+nltk.download('stopwords')
+
+print('\033[92m' + tweet)
+print('\033[94m')
+
+# remove old style retweet text "RT"
+tweet2 = re.sub(r'^RT[\s]+', '', tweet)
+
+# remove hyperlinks
+tweet2 = re.sub(r'https?://[^\s\n\r]+', '', tweet2)
+
+# remove hashtags
+# only removing the hash # sign from the word
+tweet2 = re.sub(r'#', '', tweet2)
+
+print()
+print('String without # and hiperlinks:')
+print(tweet2)
+
+print()
+print('\033[92m' + tweet2)
+print('\033[94m')
+
+# instantiate tokenizer class
+tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True,
+                               reduce_len=True)
+
+# tokenize tweets
+tweet_tokens = tokenizer.tokenize(tweet2)
+
+print()
+print('Tokenized string:')
+print(tweet_tokens)
+
+#Import the english stop words list from NLTK
+stopwords_english = stopwords.words('english')
+
+print('Stop words\n')
+print(stopwords_english)
+
+print('\nPunctuation\n')
+print(string.punctuation)
+
+print()
+print('\033[92m')
+print(tweet_tokens)
+print('\033[94m')
+
+tweets_clean = []
+
+for word in tweet_tokens: # Go through every word in your tokens list
+    if (word not in stopwords_english and  # remove stopwords
+        word not in string.punctuation):  # remove punctuation
+        tweets_clean.append(word)
+
+print('removed stop words and punctuation:')
+print(tweets_clean)
+
+# Instantiate stemming class
+stemmer = PorterStemmer()
+
+# Create an empty list to store the stems
+tweets_stem = []
+
+for word in tweets_clean:
+    stem_word = stemmer.stem(word)  # stemming word
+    tweets_stem.append(stem_word)  # append to the list
+
+print('stemmed words:')
+print(tweets_stem)
